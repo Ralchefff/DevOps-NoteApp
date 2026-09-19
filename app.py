e0b286bffcdb92d306b17a2cbee7001bc8c56fb1
Ralchev <ralchev@localhost.localdomain>
2026-09-19 16:22:41 +0300

    Initial version

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return """
+    <h1>DevOps Notes</h1>
+    <p>My first deployment project.</p>
+    """
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
